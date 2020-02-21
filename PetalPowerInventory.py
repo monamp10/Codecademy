114122d89510d7a6d74e5cd34e5d71a8b0670792
monamp10 <monamp10@yahoo.com>
2020-02-21 23:54:44 +0330

    initial commit

---
+++ PetalPowerInventory.py
@@ -0,0 +1,34 @@
+﻿import codecademylib
+import pandas as pd
+
+inventory = pd.read_csv('inventory.csv')
+print(inventory.head(10))
+
+staten_island = inventory.iloc[0:10]
+print(staten_island)
+
+product_request = staten_island.product_description
+print(product_request)
+
+seed_request = inventory[(inventory.location == 'Brooklyn')&
+                         (inventory.product_type == 'seeds')]
+print(seed_request)
+
+inventory['in_stock'] = inventory.quantity.apply(lambda x:
+                                       'True' if 
+                                       x > 0
+                                       else 'False'
+                                       )
+print(inventory)
+
+inventory['total_value'] = inventory.apply(lambda row: \
+                              row.price* row.quantity, 
+                                           axis = 1)
+print(inventory.total_value)
+
+
+combine_lambda = lambda row: \
+    '{} - {}'.format(row.product_type,
+                     row.product_description)
+
+inventory['full_description'] = inventory.apply(combine_lambda , axis = 1)
